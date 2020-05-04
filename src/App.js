bb9e117bbfb20daece8f71f19adb8feef5be2222
kiran.karki <kiran.karki@student.fh-kiel.de>
2020-05-04 08:34:03 +0200

    operation on product

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+// import React from 'react';
+// import Clock from './clock'
+// // import MyComponent from './MyComponent'
+// import ColoredBlock from './ColoredBlock'
+
+// class App extends React.Component {
+// 	render(){
+//   return (
+//     <div className="App">
+//       <h3> Learnig React</h3>
+// 	   <h4>{this.props.name}</h4>
+// 	   <Clock /><br/>
+// 	   {/* <MyComponent/> */}
+// 	   <ColoredBlock/>
+//     </div>
+//   );
+// }
+// }
+
+
 import React from 'react';
-import Clock from './clock'
-// import MyComponent from './MyComponent'
-import ColoredBlock from './ColoredBlock'
+import Products from './Products.js';
 
 class App extends React.Component {
-	render(){
-  return (
-    <div className="App">
-      <h3> Learnig React</h3>
-	   <h4>{this.props.name}</h4>
-	   <Clock /><br/>
-	   {/* <MyComponent/> */}
-	   <ColoredBlock/>
-    </div>
-  );
-}
-}
+  render() {
+    return (
+      <div>
+        <header className="App-header"><h1>My React App</h1></header>
+        <section>
+          <Products ></Products>
+        </section>
+      </div>
+    );
+  }
+} 
 
 export default App;
+
